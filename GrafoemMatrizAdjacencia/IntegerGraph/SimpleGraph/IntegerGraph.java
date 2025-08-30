5c6a591e285705cba696723003719fe5a10b488b
Eugênio Victor Erbs <eugenio.job1@gmail.com>
2025-08-30 10:46:17 -0300

    Add files via upload

---
+++ GrafoemMatrizAdjacencia/IntegerGraph/SimpleGraph/IntegerGraph.java
@@ -0,0 +1,45 @@
+//Implementação simples, considerando apenas inteiros na matriz. (ou seja não é completo)
+
+public class IntegerGraph {
+    private int numVertices;
+    private int[][] matriz;
+
+    public IntegerGraph (int numVertices) {
+        this.numVertices = numVertices;
+        matriz = new int[numVertices][numVertices];
+    }//construtor
+
+    public void addAresta (int origem, int destino) {
+        matriz[origem][destino] =1;
+    }
+
+     public void removerAresta (int origem, int destino) {
+        matriz[origem][destino] = 0;
+    }
+
+     public void addArestaNaoDirecionada (int origem, int destino) {
+        matriz[origem][destino] = 1;
+        matriz[destino][origem] = 1;     
+    }
+
+    public boolean exixteAresta (int origem, int destino) {
+        return matriz[origem][destino] == 1;
+    }
+
+    public String exibirMatriz () {
+        StringBuilder sb = new StringBuilder();
+
+        for (int l = 0; l < numVertices; l++) {
+            for (int c = 0; c < numVertices; c++) {
+                sb.append(matriz[l][c]).append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+   
+
+
+
+}//class
---
+++ GrafoemMatrizAdjacencia/IntegerGraph/SimpleGraph/MainTeste.java
@@ -0,0 +1,17 @@
+public class MainTeste {
+    public static void main(String[] args) {
+        IntegerGraph grafinho = new IntegerGraph(6);
+
+        grafinho.addAresta(0, 1);
+        grafinho.addAresta(0, 3);
+        grafinho.addAresta(1, 4);
+        grafinho.addAresta(2, 4);
+        grafinho.addAresta(2, 5);
+        grafinho.addAresta(3, 1);
+        grafinho.addAresta(4, 3);
+        grafinho.addAresta(5, 5);
+
+        System.out.println(grafinho.exibirMatriz());
+        
+    }//psvd
+}//class
